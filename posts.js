f8a197f5eec7bda256fa9c62d404279f8fef11c7
Cat_Fox_junjun <160747424+AEjunjun@users.noreply.github.com>
2026-05-09 12:46:38 +0800

    Update posts.js

--- posts.js
+++ posts.js
@@ -9,6 +9,14 @@ const POSTS_CONFIG = [
         category: '通知',
         excerpt: '欢迎大家！这是我的第一篇博客文章，介绍这个博客的用途和目标。',
         mdUrl: 'https://raw.githubusercontent.com/AEjunjun/Personal-Document-Library/main/介绍个人博客.md'
+    },
+    {
+        id: 'Helpcome',
+        title: '小工作室人员管理方案',
+        date: '2026-05-9',
+        category: '通知',
+        excerpt: '对于小工作室的管理方案',
+        mdUrl: 'https://raw.githubusercontent.com/AEjunjun/Personal-Document-Library/main/工作室/人员管理方案.md'
     }
     // 添加新文章复制下面块，注意逗号分隔
     // ,{
